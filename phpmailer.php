023a5eb0c863df43dd80decb9178f693c06c03ce
davidzubi <davidzubi93@gmail.com>
2016-10-18 07:30:48 +0000

    add addres frogafroga

--- phpmailer.php
+++ phpmailer.php
@@ -7,21 +7,21 @@ $mail->Host = 'smtp.gmail.com';                             //Specify main and b
 $mail->SMTPAuth = true;                                     //Enable SMTP authentication
 $mail->Username = 'dzubiaurreoc15dw@ikzubirimanteo.com';    //SMTP username
 $mail->Password = 'HERNANI93';                              //SMTP password
-$mail->SMTPSecure = 'tls';                                  //Enable LS encryption. 'ssl' also accepted
-$mail->Port = 587;                                          //TCP port to connect to
+$mail->SMTPSecure = 'ssl';                                  //Enable LS encryption. 'ssl' also accepted
+$mail->Port = 465;                                          //TCP port to connect to
 
-$mail->setFrom('from@example.com', 'Mailer');
-$mail->addAddress('joe@example.net', 'Joe User');           //Add a recipient
+$mail->setFrom('dzubiaurreoc15dw@ikzubirimanteo.com', 'Mailer');
+$mail->addAddress('frogafroga6@gmail.com', 'prueba prueba');           //Add a recipient //frogafroga6@gmail.com password -->pruebaphp
 //$mail->addAddress('ellen@example.com');                   //Name is optional
-$mail->addReplyTo('info@example.com', 'Information');
-$mail->addCC('cc@example.com');
-$mail->addBBC('bbc@example.com');
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bbc@example.com');
 
-$mail->addAttachment('/var/tmp/file.tar.gz');               //Add attachments
+//$mail->addAttachment('/var/tmp/file.tar.gz');               //Add attachments
 //$mail->AddAttachment('/tmp/image.jpg','new.jpg');         //Optional name
 $mail->isHTML(true);                                        //Set email format to HTML
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>inbold!</b>';
+$mail->Subject = 'Prueba PHPMailer';
+$mail->Body    = 'Esta es una prueba <b>inbold!</b>';
 $mail->AltBody = 'This is the body in plain text for non-HTL mail clients';
 
 if(!$mail->send()) {
